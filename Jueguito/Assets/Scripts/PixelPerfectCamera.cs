bad7343a05c18ebadf51070de7d6fa6cd0b36033
cas636 <castanojohan24@gmail.com>
2023-12-06 20:40:48 -0500

    init

---
+++ Jueguito/Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour
+{
+    public Vector2 resolutionNativa = new Vector2(240,160);
+
+    public static float pixelToUnits = 1.0f;
+    public static float scale = 1.0f;
+
+    private void Awake () {
+        var camara = GetComponent <Camera> ();
+        if (camara.orthographic) {
+            scale = Screen.height / resolutionNativa.y;
+            pixelToUnits *= scale;
+            camara.orthographicSize = (Screen.height/2.0f) / pixelToUnits;
+        }
+    }
+}
---
+++ Jueguito/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour
+{
+    public int textureSize = 40;
+
+    public bool scaleVer = true;
+
+    public bool scaleHor = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var newWidth = !scaleHor? 1 : Mathf.Ceil (Screen.width / (textureSize * PixelPerfectCamera.scale) );
+        var newHeight = !scaleVer? 1 : Mathf.Ceil (Screen.height / (textureSize * PixelPerfectCamera.scale) );
+
+        transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize);
+
+        GetComponent <Renderer>().material.mainTextureScale = new Vector3 (newWidth, newHeight, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
